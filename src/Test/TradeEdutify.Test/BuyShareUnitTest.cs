dec728b61d3e462da6a6af222b7638d96e7aa16e
Oykun YENAL <oykun.yenal@gmail.com>
2024-02-19 21:55:07 +0300

    Unit test scenerios started to add

---
+++ src/Test/TradeEdutify.Test/BuyShareUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeEdutify.Test
+{
+    public class BuyShareUnitTest
+    {
+        [Test]
+        public void BuyShare_Should_ReturnBadRequestAsResponse_When_UserNotAuthenticated()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Test]
+        public void BuyShare_Should_ReturnResponseFalse_When_UserHasNoPorfolio()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+
+    }
+}
